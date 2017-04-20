8273aa002c2d9f4de83442fd651aefb95504c6dd
Sean McDonnell <tua57246@temple.edu>
2017-04-20 13:18:20 -0400

    Added favicon placeholder

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,6 +9,7 @@
 
      <!-- Fonts -->
      <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+     <link rel='shortcut icon' href="//www.redditstatic.com/favicon.ico" type="image/x-icon" />
      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
      <link href="/stylesheets/custom.css" rel="stylesheet" type="text/css">
  </head>
