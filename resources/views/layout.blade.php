01fb2ff539d5efb9c57cb76a4ae1f8c2f6e8e5af
Sean McDonnell <tua57246@temple.edu>
2017-06-20 19:08:43 -0400

    _

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -11,7 +11,7 @@
      <link rel='shortcut icon' href="/images/favicon.ico" type="image/x-icon" />
      <link href="https://fonts.googleapis.com/css?family=Lato:100,300" rel="stylesheet">
      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-     <link href="/stylesheets/custom.min.css" rel="stylesheet" type="text/css">
+     <link href="/stylesheets/custom.css" rel="stylesheet" type="text/css">
      <link href="/stylesheets/grid12.min.css" rel="stylesheet" type="text/css">
  </head>
  <body class="@yield('pageClass')">
