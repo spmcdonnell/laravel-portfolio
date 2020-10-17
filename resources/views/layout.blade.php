631afd87163f2223ab9abc0130fb6a6105ee12f8
Sean McDonnell <seanmcdonnell@Seans-MacBook-Pro-2.local>
2020-10-17 11:34:30 -0700

    Editing contact info in footer, and contact form settings

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,24 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
- <head>
-     <meta charset="utf-8">
-     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-     <title>@yield('tabTitle')</title>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-     <!-- Fonts -->
-     <link rel='shortcut icon' href="/images/favicon.ico" type="image/x-icon" />
-     <link href="https://fonts.googleapis.com/css?family=Lato:100,300" rel="stylesheet">
-     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-     <link href="/stylesheets/custom.css" rel="stylesheet" type="text/css">
-     <link href="/stylesheets/grid12.min.css" rel="stylesheet" type="text/css">
- </head>
- <body class="@yield('pageClass')">
-     <header class="site-header">
+    <title>@yield('tabTitle')</title>
+
+    <!-- Fonts -->
+    <link rel='shortcut icon' href="/images/favicon.ico" type="image/x-icon" />
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,300" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <link href="/stylesheets/custom.css" rel="stylesheet" type="text/css">
+    <link href="/stylesheets/grid12.min.css" rel="stylesheet" type="text/css">
+</head>
+
+<body class="@yield('pageClass')">
+    <header class="site-header">
         <div class="wrap clear-fix">
             <div class="title-area">
-                <a href="/"><div id="temp"><img src="/images/sean-avatar.png" style="width: 57px; margin-top: 2px;"></div></a>
+                <a href="/">
+                    <div id="temp"><img src="/images/sean-avatar.png" style="width: 57px; margin-top: 2px;"></div>
+                </a>
             </div>
             <div class="primary-nav">
                 <ul>
@@ -27,36 +31,44 @@
                 </ul>
             </div>
         </div>
-     </header>
-     <div class="site-inner">
-         <main class="content">
+    </header>
+    <div class="site-inner">
+        <main class="content">
             @yield('content')
-         </main>
-     </div>
-     <footer>
+        </main>
+    </div>
+    <footer>
         <div class="wrap" id="contact">
-             <div class="inside-container">
-                 <div class="row">
+            <div class="inside-container">
+                <div class="row">
                     @if(Session::has('success'))
-                        <div class="alert-success"> {{ Session::get('success') }} </div>
+                    <div class="alert-success"> {{ Session::get('success') }} </div>
                     @endif
                     <div class="col-sm-4">
                         <h2>About this site:</h2>
                         <p>This site was created using Laravel 5 and has been my pet project
-                        for a number months. When deciding to overhaul my portfolio site,
-                        I wanted to keep things simple and functional, but still visually interesting.
-                        It's also served as an opportunity to improve my knowledge of Laravel.
-                        I plan to make to regular updates and improvements,
-                        and hope to add a blog in the near future.</p>
+                            for a number months. When deciding to overhaul my portfolio site,
+                            I wanted to keep things simple and functional, but still visually interesting.
+                            It's also served as an opportunity to improve my knowledge of Laravel.
+                            I plan to make to regular updates and improvements,
+                            and hope to add a blog in the near future.</p>
                     </div>
                     <div class="col-sm-4" style="text-align: center; position: initial;">
-                        <div class="circle" style="margin: 25px auto 57px;"><img src="/images/sean-avatar.png" style="width: 90px; margin-top: 2px;"></div>
+                        <div class="circle" style="margin: 25px auto 57px;"><img src="/images/sean-avatar.png"
+                                style="width: 90px; margin-top: 2px;"></div>
                         <div class="vertical">
-                            <a href="https://www.linkedin.com/in/mcdonnell-sean/"><img style="display: inline; margin: 0 10px -9px;" src="/images/linkedin-circle-icon.png" width="30" height="30"></a><a href="https://www.linkedin.com/in/mcdonnell-sean/">mcdonnell-sean</a>
+                            <a href="https://www.linkedin.com/in/mcdonnell-sean/"><img
+                                    style="display: inline; margin: 0 10px -9px;" src="/images/linkedin-circle-icon.png"
+                                    width="30" height="30"></a><a
+                                href="https://www.linkedin.com/in/mcdonnell-sean/">mcdonnell-sean</a>
                             <div style="height: 10px;"></div>
-                            <a href="mailto:mcdsean@gmail.com"><img style="display: inline; margin: 0 10px -5px;" src="/images/email-icon.png" width="30" height="20"></a><a href="mailto:mcdsean@gmail.com">mcdsean@gmail.com</a>
+                            <a href="mailto:sean@sean-mcdonnell.com"><img style="display: inline; margin: 0 10px -5px;"
+                                    src="/images/email-icon.png" width="30" height="20"></a><a
+                                href="mailto:sean@sean-mcdonnell.com">sean@sean-mcdonnell.com</a>
                             <div style="height: 10px;"></div>
-                            <a href="https://github.com/spmcdonnell"><img style="display: inline; margin: 0 10px -10px;" src="/images/github-icon.png" width="30" height="30"></a><a href="https://github.com/spmcdonnell">spmcdonnell</a>
+                            <a href="https://github.com/spmcdonnell"><img style="display: inline; margin: 0 10px -10px;"
+                                    src="/images/github-icon.png" width="30" height="30"></a><a
+                                href="https://github.com/spmcdonnell">spmcdonnell</a>
                         </div>
                     </div>
                     <div class="col-sm-4">
@@ -74,11 +86,12 @@
                             <input type="submit" value="Submit">
                         </form>
                     </div>
-                 </div>
-             </div>
+                </div>
+            </div>
         </div>
-     </footer>
-     <script type="text/javascript" src="/js/custom.js"></script>
-     {{--<script type="text/javascript" src="/js/nicescroll/jquery.nicescroll.min.js"></script>--}}
- </body>
+    </footer>
+    <script type="text/javascript" src="/js/custom.js"></script>
+    {{--<script type="text/javascript" src="/js/nicescroll/jquery.nicescroll.min.js"></script>--}}
+</body>
+
 </html>
